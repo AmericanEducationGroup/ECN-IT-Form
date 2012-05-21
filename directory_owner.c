4f3ae9798ac04c053e09d7b0e6e041fb7794d8b3
Mark Arbogast <mark.t.arbogast@gmail.com>
2012-05-21 14:36:38 -0400

    Mostly complete with Directory_Owners

---
+++ directory_owner.c
@@ -0,0 +1,5 @@
+int main()
+{
+	for(int i = 1; i < 203; i++)
+		printf("INSERT INTO Directory_Owner (directory, owner_position) VALUES (1, %d)", i);
+}
---
+++ directory_owner2.c
@@ -0,0 +1,16 @@
+int main() {
+	int i, num = 103;
+	for(i=0; i < 14; i++)
+	{
+		printf("-- American Education Group/Schools/Fusion Division/\n");
+		printf("--   Staff\n");
+		printf("INSERT INTO Directory_Owner (directory, owner_position) VALUES (%d, );\n\n", num++);
+		printf("--   Public\n");
+		printf("INSERT INTO Directory_Owner (directory, owner_position) VALUES (%d, );\n\n", num++);
+		printf("--   Administrators\n");
+		printf("INSERT INTO Directory_Owner (directory, owner_position) VALUES (%d, );\n\n", num++);
+		printf("--   Head of School\n");
+		printf("INSERT INTO Directory_Owner (directory, owner_position) VALUES (%d, );\n\n", num++);
+	}
+	return 0;
+}
